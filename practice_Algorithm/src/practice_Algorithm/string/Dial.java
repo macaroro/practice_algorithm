1c61bbc08be6bdc1d87db5e807977a48b800dcca
macaroro <cho_0826@naver.com>
2022-10-05 20:56:45 +0900

    2022-10-05(백준 문자열:	5622,2941,1316)

--- practice_Algorithm/src/practice_Algorithm/string/Dial.java
+++ practice_Algorithm/src/practice_Algorithm/string/Dial.java
@@ -3,6 +3,8 @@ package practice_Algorithm.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dial {
 
@@ -11,6 +13,36 @@ public class Dial {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String str = br.readLine();
+		
+		List<Integer> list = new ArrayList<>();
+		for(int i=0; i<str.length();i++) {
+			char c = str.charAt(i);
+			if(c=='A'||c=='B'||c=='C') {
+				list.add(2);
+			}else if(c=='D'||c=='E'||c=='F') {
+				list.add(3);
+			}else if(c=='G'||c=='H'||c=='I') {
+				list.add(4);
+			}else if(c=='J'||c=='K'||c=='L') {
+				list.add(5);
+			}else if(c=='M'||c=='N'||c=='O') {
+				list.add(6);
+			}else if(c=='P'||c=='Q'||c=='R'||c=='S') {
+				list.add(7);
+			}else if(c=='T'||c=='U'||c=='V') {
+				list.add(8);
+			}else if(c=='W'||c=='X'||c=='Y'||c=='Z') {
+				list.add(9);
+			}
+			
+		}
+		
+		int add=0;
+		for(int i=0;i<list.size();i++) {
+			add+= list.get(i)+1;
+			
+		}
+		System.out.println(add);
 		
 		
 	}
---
+++ practice_Algorithm/src/practice_Algorithm/string/Group_Word.java
@@ -0,0 +1,46 @@
+package practice_Algorithm.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Group_Word {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	 
+	public static void main(String[] args) throws IOException {
+ 
+		int count = 0;
+		int num = Integer.parseInt(br.readLine());
+ 
+		for (int i = 0; i < num; i++) {
+			if (check()) {
+				count++;
+			}
+		}
+		System.out.print(count);
+	}
+ 
+	public static boolean check() throws IOException {
+		boolean[] check = new boolean[26];
+		int prev = 0;
+		String str = br.readLine();
+		
+		for(int i = 0; i < str.length(); i++) {
+			int now = str.charAt(i);
+			
+			
+			if (prev != now) {		
+				if (!check[now - 'a']) {
+					check[now - 'a'] = true;
+					prev = now;	
+				}
+				else {
+					return false;	
+				}
+			}
+		}    
+		return true;
+	}
+
+}
